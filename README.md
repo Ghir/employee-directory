54d57e467b0d98c69e6fcd836b38d619df484106
Dario Ghirardo <dario.ghirardo@gmail.com>
2017-12-30 22:14:06 +0100

    Added navigation buttons

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # JS-Project-5---Use-a-public-API-to-create-an-employee-directory
 
 TREEHOUSE TECHDEGREE FULL STACK JAVASCRIPT - PROJECT 5 - USE A PUBLIC API TO CREATE AN EMPLOYEE DIRECTORY
+
+What the program does:
+
+It uses the Random User Generator API to grab information for 12 random “employees", and builds a prototype for an Awesome Startup employee directory.
+A JSON object is requested from the API and the data is parsed so that 12 employees are listed in a grid with their thumbnail image, full name, email, and location. Clicking the employee’s image or name will open a modal window with more detailed information, such as the employee’s birthday and address. The 'previous' and 'next' buttons provide navigation through the employees list. A search field can be used to filter for a specific name, email, username or location.
--- js/app.js
+++ js/app.js
@@ -1,12 +1,18 @@
+// JS-Project-5---Use-a-public-API-to-create-an-employee-directory
 
-function capitalizeFirstLetter(string) {
+!function () {
+
+function capitalizeFirstLetter (string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+let childNumber = 0;
 
 $.ajax({
   url: 'https://randomuser.me/api/?results=12&nat=gb,au,dk,es,fi,fr,nl,nz,us',
   dataType: 'json',
   success: function(data) {
+    // create rows
     for (let i = 0; i < data.results.length; i++) {
       $('li:nth-of-type(' + (i+1) + ') img').attr('src', data.results[i].picture.large);
       $('li:nth-of-type(' + (i+1) + ') .name').html(capitalizeFirstLetter(data.results[i].name.first) + ' ' + capitalizeFirstLetter(data.results[i].name.last));
@@ -14,7 +20,7 @@ $.ajax({
       $('li:nth-of-type(' + (i+1) + ') .city').html(capitalizeFirstLetter(data.results[i].location.city));
       $('li:nth-of-type(' + (i+1) + ') .username').html(data.results[i].login.username);
     }
-
+    // show modal window on click
     $('li').click(function(event) {
       const $overlay = $(`
         <div class="overlay">
@@ -36,45 +42,61 @@ $.ajax({
           </div>
         </div>
         `);
-
       $('.container').append($overlay);
+      // basic details in modal window
       $('.thumbnail2').attr('src', $('.thumbnail', this).attr('src'));
       $('.name2').html($('.name', this).text());
       $('.email2').html($('.email', this).text());
       $('.city2').html($('.city', this).text());
 
-      const childNumber = this.id;
+      childNumber = this.id;
+      // hide and show buttons when needed
+      if (parseInt(childNumber) == 0) {
+        $('#previous').hide()
+      }
+      else if (parseInt(childNumber) == 11) {
+        $('#next').hide();
+      }
+      // provide additional details in modal window
       $('.cell').html(data.results[childNumber].cell);
       $('.address').html(data.results[childNumber].location.street + data.results[childNumber].location.city + ', ' + data.results[childNumber].location.state + ', ' +  data.results[childNumber].location.postcode);
       $('.birthday').html('Birthday: ' + data.results[childNumber].dob.slice(0, 11));
       $('.username2').html('Username: ' + data.results[childNumber].login.username)
-
+      // exit modal window when click outside of it
       $overlay.click((event) => {
         if (event.target.className == 'overlay') {
           $overlay.remove();
         }
       })
+      // navigation with 'next' and 'previous' buttons
+      $('#next').click(function () {
+        $overlay.remove();
+        $('li')[parseInt(childNumber)+1].click();
+      })
+      $('#previous').click(function () {
+        $overlay.remove();
+        $('li')[parseInt(childNumber)-1].click();
+      })
     }) // end click
   } // end success function
 }); // end Ajax
-
 
 const $employees = $('li');
 const $message = $('<p>Not found...</p>');
 const $input = $('input');
 
+// search feature to filter employees
 function search () {
   matchedEmployees = [];
   $employees.hide();
   $message.remove();
-
+  // if employee matches the search, add it to a new array
   $employees.each(function () {
     if ($(this).text().includes($input.val().toLowerCase())) {
       matchedEmployees.push(this);
     }
   })
-
-  // show matched students
+  // show matched employees
   for (let i=0; i<12; i++) {
     for (let e=0; e<matchedEmployees.length; e++) {
       if ($employees[i] == matchedEmployees[e]) {
@@ -86,18 +108,21 @@ function search () {
   if (matchedEmployees.length == 0) {
     $('.wrapper').append($message);
   }
-} // end Search
+} // end search
 
 $('#search').click(search)
 
+// reset feature to clear everything
 $('#reset').click(() => {
   $input.val('');
   $message.remove();
   $employees.show()
 })
 
+// if enter key is pressed and input is active then search employees
 $(window).keydown(function(e) {
-  // if enter key is pressed and input is active then search employees
   if (e.keyCode === 13 && $(document.activeElement).is("input")) {
     search();
   }})
+
+}();
